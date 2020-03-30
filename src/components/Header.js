b45944419df1328c2d4a282d6afea2d38eff67dc
passos555 <asn94@outlook.com>
2020-03-29 23:37:31 -0300

    chore: altera estilo do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import logo from '../assets/facebook.png';
 
-
 function Header() {
   return (
-    <header>
+    <header className="main-header">
       <nav>
-        <img src={logo} alt="Facebook Logo"/>
+        <img src={logo} alt="Facebook Logo" />
+        <span>Meu Perfil</span>
       </nav>
     </header>
-  )
+  );
 }
 
 export default Header;
